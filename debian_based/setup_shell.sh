716ad8e12d8745afe530062cc2ee6406ffb10eec
searleser97 <gabrielsanv97@gmail.com>
2023-05-22 14:27:06 -0700

    add install script

---
+++ debian_based/setup_shell.sh
@@ -0,0 +1,25 @@
+# Install ZSHell
+sudo nala install zsh
+chsh -s $(which zsh)
+zsh
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo -e '# ZSH_THEME="amuse"\nZSH_THEME="jonathan"\n$(cat input)' > input
+
+# Install `brew`
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Install utilities to build stuff
+sudo nala install build-essential
+# Install neovim nightly
+brew install neovim --HEAD
+# Install NvChad and neovim dependencies
+sudo nala install ripgrep python3-venv xclip
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash # installs NVM
+nvm install node
+# Install NvChad
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+# Install my NvChad Custom Config
+git clone https://github.com/searleser97/NvChadCustomConfig ~/.config/nvim/lua/custom
+
+# reboot to be able to use the new default shell which is ZSH
+reboot
