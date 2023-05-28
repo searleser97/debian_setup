42eda2976f9da0e49a85f94d5ea8a7c6b1145596
searleser97 <gabrielsanv97@gmail.com>
2023-05-27 20:42:53 -0700

    use pacstall to install neovim nightly

--- debian_based/setup_shell.sh
+++ debian_based/setup_shell.sh
@@ -10,15 +10,11 @@ zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo -e '# ZSH_THEME="amuse"\nZSH_THEME="jonathan"\n$(cat input)' > input
 
-# Install `brew`
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-# Install utilities to build stuff
-sudo nala install build-essential
 # Install neovim nightly
-brew install neovim --HEAD
+pacstall -I neovim-git
 # Install NvChad and neovim dependencies
 sudo nala install ripgrep python3-venv xclip
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash # installs NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash # installs NVM (Node Version Manager)
 nvm install node
 # Install NvChad
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
