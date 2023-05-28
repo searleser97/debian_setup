f4063bf4dfce8f5ee0a3ae0faf5d1e8be08257a2
searleser97 <gabrielsanv97@gmail.com>
2023-05-27 21:42:48 -0700

    update

--- debian_based/setup_shell.sh
+++ debian_based/setup_shell.sh
@@ -11,7 +11,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 echo -e '# ZSH_THEME="amuse"\nZSH_THEME="jonathan"\n$(cat input)' > input
 
 # Install neovim nightly
-pacstall -I neovim-git
+sudo snap install --edge nvim --classic
 # Install NvChad and neovim dependencies
 sudo nala install ripgrep python3-venv xclip
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash # installs NVM (Node Version Manager)
@@ -20,6 +20,13 @@ nvm install node
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
 # Install my NvChad Custom Config
 git clone https://github.com/searleser97/NvChadCustomConfig ~/.config/nvim/lua/custom
+
+# Install telegram
+pacstall -I telegram-bin
+# Install grub-customizer
+sudo nala install grub-customizer
+# Install input remapper
+sudo nala install input-remapper
 
 # reboot to be able to use the new default shell which is ZSH
 reboot
