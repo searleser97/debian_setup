384265ce5518cf4aaff55001875505e6e691bbe4
searleser97 <gabrielsanv97@gmail.com>
2023-05-29 23:14:54 -0700

    update

--- debian_based/setup_shell.sh
+++ debian_based/setup_shell.sh
@@ -7,7 +7,35 @@ sudo bash -c "$(curl -fsSL https://pacstall.dev/q/install || wget -q https://pac
 # Install nala
 pacstall -I nala-deb -P
 # Install ZSHell
-sudo nala install zsh
+sudo nala install zsh -y
 chsh -s $(which zsh)
 echo '# remove this comment' > ~/.zshrc
-zsh -c "source ./setup_shell_2.sh"
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo -e "# ZSH_THEME=\"amuse\"\nZSH_THEME=\"jonathan\"\n\n$(cat ~/.zshrc)" > ~/.zshrc
+# Install neovim nightly
+sudo snap install --edge nvim --classic
+# Install NvChad and neovim dependencies
+sudo nala install ripgrep python3-venv xclip -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | zsh # installs NVM (Node Version Manager)
+source ~/.zshrc
+nvm install node
+# Install NvChad
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+# Install my NvChad Custom Config
+git clone https://github.com/searleser97/NvChadCustomConfig ~/.config/nvim/lua/custom
+# Install telegram
+pacstall -I telegram-bin -P
+# Install grub-customizer
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer
+sudo nala install grub-customizer -y
+# Install input remapper
+sudo nala install input-remapper -y
+input-remapper &;disown
+# Apply mappings
+node ./mappings_setup.cjs
+# Install chrome
+pacstall -I google-chrome-deb -P
+
+# reboot to be able to use the new default shell which is ZSH
+reboot
