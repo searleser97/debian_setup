6b682031997a765c089341ee09ed742207dfb64c
searleser97 <gabrielsanv97@gmail.com>
2023-06-14 10:59:30 -0700

    update

--- after_first_restart.sh
+++ after_first_restart.sh
@@ -3,7 +3,8 @@
 xterm -e zsh -c 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc' & disown
 # Complete NVChad installation
 xterm -e zsh -c 'nvim' & disown
+
+xterm -hold -e zsh -c 'read -p "[Press ENTER to continue]"'
 # Remove autostart script
 AUTOSTARTDIR="/etc/xdg/autostart"
 sudo rm $AUTOSTARTDIR/after_first_restart.*
-xterm -e zsh -c 'read -p "[Press ENTER to continue]"'
