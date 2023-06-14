8943a68b972db9e8fc5bc4022d2cccd1b9ab559b
searleser97 <gabrielsanv97@gmail.com>
2023-06-14 09:55:25 -0700

    update

--- after_first_restart.sh
+++ after_first_restart.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
-# Complete NVChad installation
-xterm -e 'nvim'
 # Install oh-my-zsh
 xterm -hold -e 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc'
+# Complete NVChad installation
+xterm -e 'nvim'
 # Remove autostart script
 rm ~/.config/autostart/after_first_restart.*
 xterm -hold -e 'read -p "[Press ENTER to continue]"'
