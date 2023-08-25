f68b7de272afc9574ef11f5d473abed81cb2b7f5
Sergio <gabrielsanv97@gmail.com>
2023-08-25 16:48:56 -0700

    Update after_first_restart.sh

--- after_first_restart.sh
+++ after_first_restart.sh
@@ -49,4 +49,10 @@ read A
 if [ "$ISWSL" = "no" ]; then
 # Remove autostart script
 sudo rm $AUTOSTARTDIR/after_first_restart.*
+else
+# install win32yank to share clipboard between neovim and windows 11
+curl -sLo/tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
+unzip -p /tmp/win32yank.zip win32yank.exe > /tmp/win32yank.exe
+chmod +x /tmp/win32yank.exe
+sudo mv /tmp/win32yank.exe /usr/local/bin/win32yank.exe
 fi
