7af557e89b749731fb43019996d1b9f38487784a
searleser97 <gabrielsanv97@gmail.com>
2024-09-21 22:53:27 -0700

    update

--- after_first_restart.sh
+++ after_first_restart.sh
@@ -7,15 +7,14 @@ fi
 if [ "$ISWSL" = "no" ]; then
 AUTOSTARTDIR="/etc/xdg/autostart"
 fi
-
+# Install git credential manager
+dotnet tool install -g git-credential-manager
+git-credential-manager configure
 # Install common neovim dependencies
+nvm install node
 sudo nala install ripgrep python3-venv xclip fd-find -y
 # Install neovim nightly
-mkdir ~/ProgramFiles
-wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz -P ~/ProgramFiles
-tar -xvzf ~/ProgramFiles/nvim-linux64.tar.gz -C ~/ProgramFiles/
-# Install my neovim config
-git clone https://github.com/searleser97/nvim_lua ~/.config/nvim
+brew install neovim --HEAD
 # Install VSCode
 sudo snap install code --classic
 # Add desktop entry for VSCode
@@ -24,38 +23,32 @@ cp /snap/code/current/meta/gui/code.desktop ~/.local/share/applications/
 cp /snap/code/current/meta/gui/code-url-handler.desktop ~/.local/share/applications/
 sed -i 's/${SNAP}/\/snap\/code\/current/g' ~/.local/share/applications/code.desktop
 sed -i 's/${SNAP}/\/snap\/code\/current/g' ~/.local/share/applications/code-url-handler.desktop
-# Install telegram
-pacstall -I telegram-bin -P
+# Install my settings and keybindings in the VSCode installation
+cp ~/.config/nvim/settings.json ~/.config/Code/User/settings.json
+cp ~/.config/nvim/keybindings.json ~/.config/Code/User/keybindings.json
 # Install Microsoft Edge
 curl -fSsL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor | sudo tee /usr/share/keyrings/microsoft-edge.gpg > /dev/null
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-edge.gpg] https://packages.microsoft.com/repos/edge stable main' | sudo tee /etc/apt/sources.list.d/microsoft-edge.list
 sudo nala update
 sudo nala install microsoft-edge-stable -y
-
-# save git credentials in computer
-git config --global credential.helper store
-# set nvim as default git editor
-git config --global core.editor "nvim"
-# auto create branches on remote locally
-git config --global push.autoSetupRemote true
-# set delta as default pager
-git config --global core.pager "delta"
+# Install telegram
+flatpak install telegram -y
 
 # Install flutter
-sudo nala install openjdk-17-jdk -y
-sudo snap install flutter --classic
-mkdir ~/Android
+# sudo nala install openjdk-17-jdk -y
+# sudo snap install flutter --classic
+# mkdir ~/Android
 # the url to get the latest version of command line tools can be found in https://developer.android.com/studio#command-tools
-wget https://dl.google.com/android/repository/commandlinetools-linux-10406996_latest.zip -P ~/Android
-unzip ~/Android/commandlinetools-linux-10406996_latest.zip -d ~/Android/
-mkdir ~/Android/latest/
-mv ~/Android/cmdline-tools/* ~/Android/latest
-mv ~/Android/latest ~/Android/cmdline-tools
-~/Android/cmdline-tools/latest/bin/sdkmanager "platform-tools" "platforms;android-33" "build-tools;33.0.2" "system-images;android-33;google_apis;x86_64" --sdk_root="$HOME/Android"
-export PATH=$HOME/Android/platform-tools:$PATH
-flutter doctor
-flutter doctor --android-licenses
-flutter doctor
+# wget https://dl.google.com/android/repository/commandlinetools-linux-10406996_latest.zip -P ~/Android
+# unzip ~/Android/commandlinetools-linux-10406996_latest.zip -d ~/Android/
+# mkdir ~/Android/latest/
+# mv ~/Android/cmdline-tools/* ~/Android/latest
+# mv ~/Android/latest ~/Android/cmdline-tools
+# ~/Android/cmdline-tools/latest/bin/sdkmanager "platform-tools" "platforms;android-33" "build-tools;33.0.2" "system-images;android-33;google_apis;x86_64" --sdk_root="$HOME/Android"
+# export PATH=$HOME/Android/platform-tools:$PATH
+# flutter doctor
+# flutter doctor --android-licenses
+# flutter doctor
 
 # Install oh-my-zsh
 echo "After clicking [Enter] this script will proceed to install 'oh-my-zsh' which will"
--- setup.sh
+++ setup.sh
@@ -4,34 +4,35 @@ if grep -qi microsoft /proc/version; then
   ISWSL="yes"
 fi
 
+# Install my zshrc config
+cat ./.zshrc > ~/.zshrc
+# Install my neovim config
+git clone https://github.com/searleser97/nvim_lua ~/.config/nvim
+# Install my wezterm settings
+cp ~/.config/nvim/.wezterm.config ~/.wezterm.config
+# Install my gitconfig settings
+cp ~/.config/nvim/.gitconfig ~/.gitconfig
 # Install pacstall
 sudo bash -c "$(curl -fsSL https://pacstall.dev/q/install || wget -q https://pacstall.dev/q/install -O -)"
 # Install nala
 pacstall -I nala-deb -P
+# Install Wezterm
 curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
 echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
-sudo apt-get update
-sudo apt-get install wezterm
+sudo nala update
+sudo nala install wezterm -y
 # Install ZSHell
 sudo nala install zsh -y
+# set ZShell as default terminal
 chsh -s $(which zsh)
-# Install nodejs using nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash # installs NVM (Node Version Manager)
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-nvm install node
-
-cat ./my_zshrc > ~/.zshrc
-
+# Install dotnet tools to be able to install git credential manager after reboot
+sudo nala install dotnet-sdk-7.0
+# Install node version manager to be able to install nodejs after reboot
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 if [ "$ISWSL" = "no" ]; then
 # Install grub-customizer
 sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
 sudo nala install grub-customizer -y
-# Install touchpad drivers
-sudo nala install xserver-xorg-input-synaptics -y
 # Install wacom tablet settings gui
 sudo nala install kde-config-tablet -y
 # Install nordvpn
@@ -49,7 +50,7 @@ echo "creating temporary autostart file in $AUTOSTARTDIR"
 sudo mkdir $AUTOSTARTDIR
 sudo cp after_first_restart.desktop $AUTOSTARTDIR
 chmod +x ./after_first_restart.sh
-echo "Exec=xterm -e bash -c 'cd $(pwd) && source ./after_first_restart.sh' &" | sudo tee -a $AUTOSTARTDIR/after_first_restart.desktop
+echo "Exec=wezterm start zsh -c \"cd $(pwd) && source ./after_first_restart.sh; zsh -i'\" | sudo tee -a $AUTOSTARTDIR/after_first_restart.desktop
 
 echo "The stage 1 of the setup has completed !"
 echo "Click [Enter] to reboot your machine and continue with the final stage of the setup"
