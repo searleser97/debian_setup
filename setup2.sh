772f55dcee0ac898d99f3ff9dd850cd4cdac86b2
Sergio <gabrielsanv97@gmail.com>
2025-07-21 08:53:21 -0700

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -86,7 +86,7 @@ fi
 
 if [ "$ISWSL" = "yes" ]; then
 # install win32yank to share clipboard between neovim and windows 11
-curl -sLo/tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
+curl -sLo/tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.1.1/win32yank-x64.zip
 unzip -p /tmp/win32yank.zip win32yank.exe > /tmp/win32yank.exe
 chmod +x /tmp/win32yank.exe
 sudo mv /tmp/win32yank.exe /usr/local/bin/win32yank.exe
