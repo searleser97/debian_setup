33a0f9e4cc4342a3b94e5676bd64ca839e9c274d
Sergio <gabrielsanv97@gmail.com>
2025-07-28 12:43:36 -0700

    Update setup1.sh

--- setup1.sh
+++ setup1.sh
@@ -6,7 +6,7 @@ fi
 # Install dotnet tools to be able to install git credential manager after reboot
 wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
 chmod +x ./dotnet-install.sh
-./dotnet-install.sh --version 8.0
+./dotnet-install.sh --channel 8.0
 ./dotnet-install.sh --channel 9.0
 # Install node version manager to be able to install nodejs after reboot
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
