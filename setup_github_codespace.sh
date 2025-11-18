a5bfcdc64beaffe75cfce3141300201ded4c260a
Sergio <gabrielsanv97@gmail.com>
2025-11-17 16:23:16 -0800

    Update setup_github_codespace.sh

--- setup_github_codespace.sh
+++ setup_github_codespace.sh
@@ -37,3 +37,13 @@ sudo nala install tmux -y
 mkdir ~/.copilot
 cat ./copilot-instructions.md > ~/.copilot/copilot-instructions.md
 npm install -g @github/copilot
+# install github cli
+(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
