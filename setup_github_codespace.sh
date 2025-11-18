2b6e0c74d8b5141e39c340fa336cb92d5619000d
Sergio <gabrielsanv97@gmail.com>
2025-11-18 11:49:21 -0800

    Update setup script for GitHub Codespace
    Backup existing sources lists, install utilities, and configure Neovim and zsh.

--- setup_github_codespace.sh
+++ setup_github_codespace.sh
@@ -1,39 +1,44 @@
+sudo mv /etc/apt/sources.list.d/ubuntu.sources /etc/apt/sources.list.d/ubuntu.sources.bak
+sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu/ noble main restricted universe multiverse" --yes
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu/ noble-updates main restricted universe multiverse" --yes
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu/ noble-security main restricted universe multiverse" --yes
-sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt-get update
 
 # Install nala
-curl https://gitlab.com/volian/volian-archive/-/raw/main/install-nala.sh | bash
-sudo apt install -t nala nala
+sudo apt-get install nala -y
+# Install utilities
+sudo nala install ripgrep python3-venv wl-clipboard fd-find tmux jq neovim software-properties-common -y
 
-sudo chsh "$(id -un)" --shell $(which zsh)
-
+# Install Oh-My-Zsh
 export RUNZSH="no"
-sudo add-apt-repository ppa:neovim-ppa/unstable -y
-
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
-# Install my zshrc config
-cat ./.zshrc > ~/.zshrc
-# Install tmux config
-cat ./.tmux.conf > ~/.tmux.conf
+sudo chsh "$(id -un)" --shell $(which zsh)
 # Install zsh-nvm to load nvm lazily (more details in the .zshrc file)
 mkdir -p ~/.oh-my-zsh/custom/plugins/
 git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
-# Install my neovim config
+# Install my zshrc config
+cat ./.zshrc > ~/.zshrc
+
+# Install tmux config
+cat ./.tmux.conf > ~/.tmux.conf
+
+# Setup Neovim
 git clone https://github.com/searleser97/nvim_lua ~/.config/nvim
 mkdir ~/.local/share/nvim/sessions
+
 # Install my gitconfig settings
 cp ~/.config/nvim/.gitconfig ~/.gitconfig
-# Install utilities
-sudo nala install ripgrep python3-venv wl-clipboard fd-find tmux jq neovim software-properties-common -y
+
 # Install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 cargo install git-delta
 
+# Install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+
 # Roslyn lsp server for .NET C# requires more watch instances than the default in linux
 echo "fs.inotify.max_user_instances=8192" | sudo tee -a /etc/sysctl.conf 
 echo "fs.inotify.max_user_watches=1048576" | sudo tee -a /etc/sysctl.conf
@@ -50,7 +55,7 @@ sudo sysctl -p
 	&& sudo apt update \
 	&& sudo apt install gh -y
 
-
+# Install Copilot CLI
 mkdir ~/.copilot
 cat ./copilot-instructions.md > ~/.copilot/copilot-instructions.md
 npm install -g @github/copilot
