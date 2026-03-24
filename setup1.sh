41c0f275f84e5fda7323f9544a6f9f46574fd21e
Sergio <gabrielsanv97@gmail.com>
2026-03-24 14:43:03 -0700

    Simplify setup script by removing WSL logic
    Removed WSL check and related installation commands.

--- setup1.sh
+++ setup1.sh
@@ -1,9 +1,4 @@
 set -e
-ISWSL="no"
-
-if grep -qi microsoft /proc/version; then
-  ISWSL="yes"
-fi
 
 export NVM_DIR="$HOME/.nvm"
 source "$NVM_DIR/nvm.sh"
@@ -21,13 +16,3 @@ dotnet tool install -g git-credential-manager
 git-credential-manager configure
 # looks like now, the installation command also asks me to login already
 # az login
-if [ "$ISWSL" = "no" ]; then
-# Install grub-customizer
-sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
-sudo nala install grub-customizer -y
-# Install wacom tablet settings gui
-# sudo nala install kde-config-tablet -y
-# Install nordvpn
-sh <(wget -qO - https://downloads.nordcdn.com/apps/linux/install.sh) -p nordvpn-gui
-sudo usermod -aG nordvpn $USER
-fi
