ba4bce0ff1872822bcbf58816e9c31ff3f11ba21
Sergio <gabrielsanv97@gmail.com>
2026-03-24 13:21:11 -0700

    Simplify Azure CLI installation in setup.sh
    Replaced manual installation of Azure CLI with a single command for simplicity.

--- setup.sh
+++ setup.sh
@@ -81,20 +81,9 @@ if [ "$ISWSL" = "yes" ]; then
 	
 	# to be able to restore (install dependencies) dotnet solutions (projects)
 	echo "downloading & installing azure cli deb"
-	sudo mkdir -p /etc/apt/keyrings
-	curl -sLS https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/keyrings/microsoft.gpg > /dev/null
-	sudo chmod go+r /etc/apt/keyrings/microsoft.gpg
-	AZ_DIST=$(lsb_release -cs)
-	echo "Types: deb
-	URIs: https://packages.microsoft.com/repos/azure-cli/
-	Suites: ${AZ_DIST}
-	Components: main
-	Architectures: $(dpkg --print-architecture)
-	Signed-by: /etc/apt/keyrings/microsoft.gpg" | sudo tee /etc/apt/sources.list.d/azure-cli.sources
+	curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 	echo "downloading & installing microsoft credprovider"
 	wget -qO- https://aka.ms/install-artifacts-credprovider.sh | bash
-	sudo apt-get update
-	sudo apt-get install azure-cli
 fi
 
 # Install Oh-My-ZSH
