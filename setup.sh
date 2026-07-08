1b3f8dc7d5225e1079a84e8276dd3dbe62f4048e
Sergio <gabrielsanv97@gmail.com>
2026-07-08 11:02:05 -0700

    Update zsh command to execute setup1.sh directly
    Change the way setup1.sh is sourced in zsh.

--- setup.sh
+++ setup.sh
@@ -135,7 +135,8 @@ if [ ! -f "$INSTALL_MARKER" ]; then
 	touch "$INSTALL_MARKER"
 fi
 
-zsh -i -c "source ~/debian_setup/setup1.sh; exec zsh;"
+# zsh -i -c "source ~/debian_setup/setup1.sh; exec zsh;"
+exec zsh ~/debian_setup/setup1.sh
 
 if [ "$ISWSL" = "no" ] && [ "$IS_CODESPACES" = "false"] ; then
 	# Install grub-customizer
